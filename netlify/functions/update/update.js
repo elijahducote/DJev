707108ba649a4fcf784e701928b2b61849ac6256
elijahducote <validpupflower@gmail.com>
2023-07-29 10:54:11 -0500

    update netlify/functions/update/update.js

--- netlify/functions/update/update.js
+++ netlify/functions/update/update.js
@@ -1,101 +1,130 @@
-exports.handler = async function (event, context, callback) {
-  try {
-    console.log(event)
-    var body = JSON.parse(event.body)
-  }
-  catch (e) {
-    console.log(event)
-    callback(
-      e.message,
-      {
-        statusCode: 400,
-        body: `[ERROR] Invalid JSON - ${e.message}`
-      }
-    )
-    return
-  }
- 
-  var uri = body.content + "&raw=1",
-  encoder = require("@rossbob/image-to-base64"),
-  URL = require("url"),
-  https = require("https"),
-  fyl = URL.parse("https://api.github.com/repos/elijahducote/djev/contents/blob/main/img/newest.png"),
-  cfg = {hostname:fyl.hostname,path:fyl.pathname,method:'PUT',headers:{'Content-Type': 'application/vnd.github+json','Accept':'application/vnd.github+json','Authorization':`Bearer ${process.env.TOKEN}`,'X-GitHub-Api-Version':'2022-11-28'}};
-  async function main(str) {
-  const imageBase64WithURI = await encoder.toBase64({uri:str})
-  return imageBase64WithURI;
-  }
+import fetch from "node-fetch";
+import {arrayBufferToBinaryString} from "blob-util";
 
-  main(uri).then((dt) => {
-  body.content = dt;
-  }).catch(console.error);
+var body,
+adm = require("adm-zip"),
+zip,
+sample_png = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQYV2NgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII=",
+axios = require("axios"),
+git_path = "https://api.github.com/repos/elijahducote/djev/contents/img/newest.png",
+Dropbox = require("dropbox").Dropbox,
+ketch = require("isomorphic-fetch"),
+sendr = {"Host":"api.github.com","Accept":"application/vnd.github+json","X-GitHub-Api-Version":"2022-11-28","Authorization":`${process.env.TOKEN}`};
 
-  async function getFylHash() {
-  var data = "",
-  options = {hostname:fyl.hostname,path:fyl.pathname,method:'GET',headers:{'Content-Type': 'application/vnd.github+json','Accept':'application/vnd.github+json','Authorization':`Bearer ${process.env.TOKEN}`,'X-GitHub-Api-Version':'2022-11-28'}},
-  responseBody = '';
+exports.handler = async function (event) {
+  body = JSON.parse(event.body);
   
-  let p = new Promise((resolve, reject) => {
-        const req = https.request(options, (res) => {
-            res.setEncoding('utf8');
-
-            res.on('data', (chunk) => {
-                responseBody += chunk;
-            });
-
-            res.on('end', () => {
-                resolve(JSON.parse(responseBody));
-            });
-        });
-
-        req.on('error', (err) => {
-            reject(err);
-        });
-
-        req.write(data)
-        req.end();
-    });
-
-    return await p;
-  }
-  await getFylHash().then((bdy) => {
-    body.ssh = bdy.ssh;
+  var req = await fetch(git_path, {
+    method:"GET",
+    headers:sendr
+  }).then((res) => {
+  
+   var fyl = res.json();
+   body.sha = fyl.sha;
+  
   });
   
+  var dbx = new Dropbox({accessToken:`${process.env.TOKEN}`,fetch: ketch});
+  dbx.filesDownload({path:"/Newest"}).then(function(resp) {
+      var blob = resp.result.fileBinary;
+      zip = new adm(blob);
+     //console.log(arrayBufferToBinaryString(blob))
+    })
+  return {statusCode:200,body:JSON.stringify({message:"DELIVERED."})};
+};
+
+
+const MODES = {FILE:"100644",FOLDER:"040000"},
+TYPE = {BLOB:"blob",TREE:"tree"},
+FILES_TO_COMMIT = [
+  {
+    path: "img/newest.png",
+    content: sample_png
+  },
+  {
+    path: "img/newer.png",
+    content: sample_png
+  },
+   {
+    path: "img/new.png",
+    content: sample_png
+  },
+  {
+    path: "img/old.png",
+    content: sample_png
+  },
+  {
+    path: "img/older.png",
+    content: sample_png
+  },
+  {
+    path: "img/oldest.png",
+    content: sample_png
+  }
+],
+COMMITS_URL = "https://api.github.com/repos/elijahducote/djev/git/commits",
+REPOSITORY_TREE_URL = "https://api.github.com/repos/elijahducote/djev/git/trees",
+REF_URL = "https://api.github.com/repos/elijahducote/djev/git/refs/heads/main";
+
+const main = async () => {
+  var ntries = zip.getEntries(),
+  obj = [],
+  len;
   
-  const req = https.request(cfg, function(res) {
-    console.log(`Status: ${res.statusCode}`);
-    console.log(`Headers: ${JSON.stringify(res.headers)}`)
-    res.setEncoding('utf8');
+  ntries.forEach(function (entry) {
+   var elm = obj.push({}),
+   format = entry.header.toJSON();
+   elm.dub = entry.entryName;
+   elm.tym = format.time;
+  });
+  obj.sort(function(a,b){
+   return b.tym.getTime() - a.tym.getTime();
+  });
+  
+  for (var len = 6;len;--len) {
+   var data = zip.getEntry(FILES_TO_COMMIT[len - 1].dub),
+   procure = data.readAsText();
+   FILES_TO_COMMIT[len - 1].content = arrayBufferToBinaryString(procure);
+  }
+  
+  const { data: { object: { sha: currentCommitSha } } } = await axios({ url: REF_URL, headers: sendr });
+  const COMMIT_URL = `${COMMITS_URL}/${currentCommitSha}`;
+  const { data: { tree: { sha: treeSha } } } = await axios({ url: COMMIT_URL, headers: sendr });
 
-    // Log data
-    res.on('data', function (body) {
-      console.log(`Body: ${body}`);
-    });
-  })
+  const { data: { sha: newTreeSha } } = await axios({
+    url: REPOSITORY_TREE_URL,
+    method: "POST",
+    headers:sendr,
+    data: {
+      base_tree: treeSha,
+      tree: FILES_TO_COMMIT
+        .map(({ content, path }) => (
+          content
+            ? { path, content, mode: MODES.FILE, type: TYPE.BLOB } // Works for text files, utf-8 assumed
+            : { path, sha: null, mode: MODES.FILE, type: TYPE.BLOB } // If sha is null => the file gets deleted
+        )),
+    },
+  });
 
-  // Handle webhook request error
-  req.on('error', function(e) {
-    const errorMessage = `[ERROR] Problem with request: ${e.message}`
-    console.log(errorMessage)
+  const { data: { sha: newCommitSha } } = await axios({
+    url: COMMITS_URL,
+    method: "POST",
+    headers:sendr,
+    data: {
+      message: "Committing with GitHub's API :fire:",
+      tree: newTreeSha,
+      parents: [ currentCommitSha ],
+    },
+  });
 
-    callback(
-      e.message,
-      {
-        statusCode: 400,
-        body: errorMessage
-      }
-    )
-  })
+  await axios({
+    url: REF_URL,
+    method: "POST",
+    headers:sendr,
+    data: { sha: newCommitSha },
+  });
+};
 
-  // Send form data to webhook request and end request
-  req.end(JSON.stringify(body))
 
-  callback(
-    null,
-    {
-      statusCode: 200,
-      body: `[SUCCESS] Sending webhook to ${fyl.format()}`
-    }
-  );
-}
+main()
+  .catch((error) => console.log(error.response.data));
