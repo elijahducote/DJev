efa61578cb9e92c3a8c022cb53c22196dba993ce
elijahducote <validpupflower@gmail.com>
2023-07-31 12:05:20 -0500

    update netlify/functions/go.js

--- netlify/functions/go.js
+++ netlify/functions/go.js
@@ -1,24 +1,8 @@
 const axios =  require("axios");
 exports.handler = async function (event, context)  {
-var fyl;
 try {
-  //const {content} = JSON.parse(event.body);
-  await axios({
-  method:"post",
-  url:"https://content.dropboxapi.com/2/files/download_zip",
-  headers: {
-    "Authorization":`Bearer ${ACCESS_TOKEN}`,
-    "Dropbox-API-Arg":"{\"path\":\"/Newest/\"}",
-    "Host":"content.dropboxapi.com"
-  },
-  params: {
-    path:"/Newest/"
-  }
-}).then((res) => {
-  fyl = JSON.stringify(res)
-})
-console.log(fyl);
-return {statusCode:200,body:JSON.stringify({message:fyl})};
+  const {content} = JSON.parse(event.body);
+return {statusCode:200,body:JSON.stringify({message:content})};
 } catch (error) {
     console.log(error);
     return {
