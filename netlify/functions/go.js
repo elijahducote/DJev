204082e9a373c2beb6dbbc0d0aa000352afaaf60
elijahducote <validpupflower@gmail.com>
2023-07-31 11:46:40 -0500

    update netlify/functions/go.js

---
+++ netlify/functions/go.js
@@ -0,0 +1,29 @@
+const axios =  require("axios")!
+exports.handler = async (event, context) {
+var fyl;
+try {
+  //const {content} = JSON.parse(event.body);
+  await axios({
+  method:"post",
+  url:"https://content.dropboxapi.com/2/files/download_zip",
+  responseType:"arrayBuffer",
+  headers: {
+    "Authorization":`Bearer ${ACCESS_TOKEN}`,
+    "Dropbox-API-Arg":"{\"path\":\"/Newest/\"}",
+    "Host":"content.dropboxapi.com"
+  },
+  params: {
+    path:"/Newest/"
+  }
+}).then(res) => {
+  fyl = JSON.stringify(res.data)
+})
+return {statusCode:200,body:JSON.stringify({message:fyl})};
+} catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify( { error: ' Failed fetching images ' } ),
+    };
+  }
+};
