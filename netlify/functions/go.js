6a8da61862c49dfb3abb4c73767ee916c23a881f
elijahducote <validpupflower@gmail.com>
2023-08-01 07:38:39 -0500

    update netlify/functions/go.js

--- netlify/functions/go.js
+++ netlify/functions/go.js
@@ -1,5 +1,5 @@
 const { Dropbox } = require('dropbox'),
-const adm = require('adm-zip'),
+adm = require('adm-zip'),
 dbx = new Dropbox({ 
   clientId: 'gm4dyloi7rntol5',
   clientSecret: process.env.APP_SECRET,
