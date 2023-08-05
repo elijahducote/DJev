924d4a8f4dca0b83016a5da6df0f8c6a14843e41
elijahducote <validpupflower@gmail.com>
2023-08-05 08:01:35 -0500

    update netlify/functions/go.js

--- netlify/functions/go.js
+++ netlify/functions/go.js
@@ -11,9 +11,7 @@ hash;
 exports.handler = async (event, context) => {
 const {content} = JSON.parse(event.body),
 path = url.parse(content),
-file = url.pathname,
-name = file.split("/"),
-dir = name[name.length - 1];
+file = url.pathname;
 
 
 const { data: response } = await axios.get("https://api.github.com/repos/elijahducote/djev/contents/img/newest.png",{headers:{"Accept":"application/vnd.github+json","Authorization":`Bearer ${process.env.TOKEN}`,"X-GitHub-Api-Version":"2022-11-28"}});
@@ -26,7 +24,7 @@ hash = response.sha;
 
 //await axios.put("https://api.github.com/repos/elijahducote/djev/contents/img/newest.png",{"message":"update file", "sha":hash,"content":img},{headers:{"Accept":"application/vnd.github+json","Authorization":`Bearer ${process.env.TOKEN}`,"X-GitHub-Api-Version":"2022-11-28"}});
 try {
-return {statusCode:200,body:JSON.stringify({success:true,resp:dir})};
+return {statusCode:200,body:JSON.stringify({success:true,resp:file})};
 } catch (error) {
     console.log(error);
     return {
