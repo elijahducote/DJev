dd82010b88ab75a03a6358fd1d60db37f21022b5
elijahducote <validpupflower@gmail.com>
2023-08-05 07:31:43 -0500

    update netlify/functions/go.js

--- netlify/functions/go.js
+++ netlify/functions/go.js
@@ -17,7 +17,7 @@ if (!file.length) file = arr[arr.length - 2];
 
 const { data: response } = await axios.get("https://api.github.com/repos/elijahducote/djev/contents/img/newest.png",{headers:{"Accept":"application/vnd.github+json","Authorization":`Bearer ${process.env.TOKEN}`,"X-GitHub-Api-Version":"2022-11-28"}});
 
-hash = response;
+hash = response.sha;
 
 //await dbx.filesDownload({path: `/Newest/${file}`})
 //  .then(async (response) => {
