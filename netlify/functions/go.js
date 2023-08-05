c67c624e639789eecb52c39ccd4b205124a9f81f
elijahducote <validpupflower@gmail.com>
2023-08-05 08:28:58 -0500

    update netlify/functions/go.js

--- netlify/functions/go.js
+++ netlify/functions/go.js
@@ -13,18 +13,17 @@ const {content} = JSON.parse(event.body),
 path = url.parse(`${content}`,true),
 names = path.pathname.split("/"),
 dir = names[names.length - 1];
-//file = url.pathname;
 
 
 const { data: response } = await axios.get("https://api.github.com/repos/elijahducote/djev/contents/img/newest.png",{headers:{"Accept":"application/vnd.github+json","Authorization":`Bearer ${process.env.TOKEN}`,"X-GitHub-Api-Version":"2022-11-28"}});
 
 hash = response.sha;
 
-//await dbx.filesDownload({path: `/Newest/${dir}`}).then(async (response) => {
-//     img = Buffer.from(response.result.fileBinary).toString("base64");
-//});
+await dbx.filesDownload({path: `/Newest/${dir}`}).then(async (response) => {
+     img = Buffer.from(response.result.fileBinary).toString("base64");
+});
 
-//await axios.put("https://api.github.com/repos/elijahducote/djev/contents/img/newest.png",{"message":"update file", "sha":hash,"content":img},{headers:{"Accept":"application/vnd.github+json","Authorization":`Bearer ${process.env.TOKEN}`,"X-GitHub-Api-Version":"2022-11-28"}});
+await axios.put("https://api.github.com/repos/elijahducote/djev/contents/img/newest.png",{"message":"update file", "sha":hash,"content":img},{headers:{"Accept":"application/vnd.github+json","Authorization":`Bearer ${process.env.TOKEN}`,"X-GitHub-Api-Version":"2022-11-28"}});
 try {
 return {statusCode:200,body:JSON.stringify({success:true,resp:dir})};
 } catch (error) {
