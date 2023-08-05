1183cdeecc9825812c9b07109fca8a4c43e9afef
elijahducote <validpupflower@gmail.com>
2023-08-05 08:12:41 -0500

    update netlify/functions/go.js

--- netlify/functions/go.js
+++ netlify/functions/go.js
@@ -9,9 +9,9 @@ dbx = new Dropbox({
 var img,
 hash;
 exports.handler = async (event, context) => {
-const {content} = JSON.parse(event.body),
-path = url.parse(content),
-file = url.pathname;
+const {content} = JSON.parse(event.body);
+//path = url.parse(content),
+//file = url.pathname;
 
 
 const { data: response } = await axios.get("https://api.github.com/repos/elijahducote/djev/contents/img/newest.png",{headers:{"Accept":"application/vnd.github+json","Authorization":`Bearer ${process.env.TOKEN}`,"X-GitHub-Api-Version":"2022-11-28"}});
@@ -24,7 +24,7 @@ hash = response.sha;
 
 //await axios.put("https://api.github.com/repos/elijahducote/djev/contents/img/newest.png",{"message":"update file", "sha":hash,"content":img},{headers:{"Accept":"application/vnd.github+json","Authorization":`Bearer ${process.env.TOKEN}`,"X-GitHub-Api-Version":"2022-11-28"}});
 try {
-return {statusCode:200,body:JSON.stringify({success:true,resp:file})};
+return {statusCode:200,body:JSON.stringify({success:true,resp:hash})};
 } catch (error) {
     console.log(error);
     return {
