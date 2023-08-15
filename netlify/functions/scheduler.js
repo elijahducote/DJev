435cb6df25b2c0897108b2cd66c54e3b28e5e277
elijahducote <validpupflower@gmail.com>
2023-08-15 09:31:42 -0500

    update netlify/functions/scheduler.js

--- netlify/functions/scheduler.js
+++ netlify/functions/scheduler.js
@@ -42,7 +42,7 @@ if (honor.day() === dayt) kairos[0] = true;
 if (hr >= relatym) kairos[1] = true;
 if (hr <= (relatym + 1)) kairos[2] = true;
 
-if (kairos[0] && kairos[1] && kairos[2]) {
+if (kairos[0] && kairos[1]) {
 
 await dbx.filesListFolder({path:`/SMM/Instagram/${today}`,include_deleted:false}).then(async (response) => {
    tab = response.result.entries;
