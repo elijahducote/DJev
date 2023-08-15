54ffd9ae68888e9e30a829fa6ccc4dde4e51b2a4
elijahducote <validpupflower@gmail.com>
2023-08-15 15:44:49 -0500

    update netlify/functions/scheduler.js

--- netlify/functions/scheduler.js
+++ netlify/functions/scheduler.js
@@ -60,7 +60,7 @@ if (dayjs(latest).isSame(assort[ndx - 1])) break;
 var fyl = tab[ndx - 1].name;
 }
 try {
-return {statusCode:200,body:JSON.stringify({success:fyl})};
+return {statusCode:200,body:JSON.stringify({success:dayjs(latest).isSame(assort[0])})};
 } catch (error) {
     return {
       statusCode: 500,
