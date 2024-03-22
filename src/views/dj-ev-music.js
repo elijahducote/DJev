0254c2389ae6fa9eccb9ee724d9742b5096ba746
elijahducote <validpupflower@gmail.com>
2024-03-22 04:52:55 -0500

    update src/views/dj-ev-music.js

--- src/views/dj-ev-music.js
+++ src/views/dj-ev-music.js
@@ -261,7 +261,7 @@ mnu.style.display = "none";
       <div className="d-ev-music-gallery">
         <div className="d-ev-music-container06 vignette">
           <div className="d-ev-music-container07">
-            <span className="d-ev-music-text20">3-24 @ 6-10P CDT</span>
+            <span className="d-ev-music-text20">3-3-24 @ 6-10P CDT</span>
             <h1 className="d-ev-music-text21">Dan Electro’s</h1>
           </div>
         </div>
