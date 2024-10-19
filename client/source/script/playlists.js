e71d2dd07432f0bd4a7430d3d6c6861c0e6f6714
elijahducote <validpupflower@gmail.com>
2024-10-19 06:28:19 -0500

    update

--- client/source/script/playlists.js
+++ client/source/script/playlists.js
@@ -20,6 +20,12 @@ export function Playlists() {
     
     htm(htm(undefined,"iframe",{src: "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F1749603915&show_artwork=true&show_comments=false",style: "width: 175%; height: 100%; position: absolute; border: 0px;",allowfullscreen:"",scrolling: "no"}),
     
+    "div",
+      {
+        style: "width: 36%; height:16em; position: relative;"
+      }
+    ),
+    
     htm(undefined,
       "br"
     ),
